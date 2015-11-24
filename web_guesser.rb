66fcefe3dcc77f541ab0768847e036432bca0aab
Steve Pentler <stevepentler@gmail.com>
2015-11-23 23:47:48 -0600

    Guessing game complete with color and cheat extensions

--- web_guesser.rb
+++ web_guesser.rb
@@ -2,21 +2,39 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'pry'
 
-number = rand(100)
+SECRET_NUMBER = (rand(100) + 1)
 
 get '/' do 
-  guess = params['guess'].to_i
-  message = check_guess(guess, number)
-  erb :index, :locals => {:number => number, :message => message}
+  guess = params['guess']
+  cheat = params['cheat']
+  message = check_guess(guess, cheat)
+  erb :index, :locals => {:number => SECRET_NUMBER, :message => message}
 end 
 
-
-def check_guess(guess = 0, number)
-  if guess == number
-    "The SECRET NUMBER is #{number}!"
+def check_guess(guess, cheat)
+  number = SECRET_NUMBER
+  guess = guess.to_i
+  if guess.zero?
+    "Please enter a guess!"
+  elsif cheat == "true"
+    "Psst! Ms. Cleo recommends #{number}!"
+  elsif guess == number 
+    "THE SECRET NUMBER IS #{number}"
   elsif guess > number
     (guess - number) > 5 ? "Way too high!" : "Too high!"
   elsif guess < number
     (number - guess)> 5 ? "Way too low!" : "Too low!"
   end 
 end 
+
+def check_background_color(message)
+  if message.split.include?("enter")
+    "white"
+  elsif message.split.include?("Way")
+    "red"
+  elsif message.split.include?("Too")
+    "pink"
+  elsif message.split.include?("SECRET")
+    "green"
+  end
+end
